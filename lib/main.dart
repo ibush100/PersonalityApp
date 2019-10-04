e92c7efbc2607096c1ce5825015d970a7c8afce4
Ian Bush <ianfracnisbush@gmail.com>
2019-10-04 08:56:51 -0400

    getting ready to migrate to statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,27 @@ void main () => runApp(MyApp());
 
 
 class MyApp extends  StatelessWidget{
+  var questionIndex = 0;
+  void answerQuestion() {
+   questionIndex = questionIndex + 1;
+   print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
     // TODO: implement build
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text('myfirstApp'),),
       body: Column(
         children:[
-          Text('the question'),
-          RaisedButton (child: Text('Answer 1'), onPressed: null,),
-         RaisedButton (child: Text('Answer 2'), onPressed: null),
+          Text(questions[questionIndex]),
+          RaisedButton (child: Text('Answer 1'), onPressed: answerQuestion,),
+         RaisedButton (child: Text('Answer 2'), onPressed: () => print('answer 2')),
          RaisedButton (child: Text('Answer 3'),onPressed: null)
         ],)
 
