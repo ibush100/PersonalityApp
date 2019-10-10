cb8b31c6c097c5110ba73cfc803539f76dfe26f9
Ian Bush <ianfracnisbush@gmail.com>
2019-10-10 08:34:37 -0400

    question updates now

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,24 @@ import 'package:flutter/material.dart';
 void main () => runApp(MyApp());
 
 
-class MyApp extends  StatelessWidget{
-  var questionIndex = 0;
-  void answerQuestion() {
-   questionIndex = questionIndex + 1;
-   print(questionIndex);
+class MyApp extends  StatefulWidget{
+
+    @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
   }
+  }
+
+
+  class MyAppState extends State <MyApp>{
+var questionIndex = 0;
+void answerQuestion() {
+  setState(() {
+    questionIndex = questionIndex + 1;
+  });
+  print(questionIndex);
+}
 
   @override
   Widget build(BuildContext context) {
